e07da0d2ffab7ac6f2a5a12e16e82965454edaaa
Chumbinho1 <vitorchumbinho123@gmail.com>
2021-11-28 19:32:45 -0300

    Adição da função de excluir uma tarefa especifica

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ onload = () => {
             let checkbox
             let task 
             let divContent
+            let trash
             let br
             
             listTasks.innerHTML = ""
@@ -22,23 +23,29 @@ onload = () => {
                 task = document.createTextNode(tasks[i]["task"])
 
                 li = document.createElement("li")
-                li.setAttribute("class", "task")
+                li.className = "taskLi"
                 li.appendChild(task)
 
                 checkbox = document.createElement("input")
                 checkbox.type = "checkbox"
                 checkbox.className = "checkbox"
-                checkbox.setAttribute("value", tasks[i]["id"])
+                checkbox.value = tasks[i]["id"]
                 checkbox.checked = tasks[i]["checkbox"]
                 checkbox.onclick = function(){check(this)}
 
+                trash = document.createElement("i")
+                trash.className = "fas fa-trash-alt"
+                trash.setAttribute("value", i) 
+                trash.onclick = function(){removeSpecificTask(this)}
+                
                 divContent = document.createElement("div")
-                divContent.className = "content"
-                divContent.appendChild(checkbox)
+                divContent.className = "task"
                 divContent.appendChild(li)
+                divContent.appendChild(trash)
 
                 br = document.createElement("br")
 
+                listTasks.appendChild(checkbox)
                 listTasks.appendChild(divContent)
                 listTasks.appendChild(br)
                 
@@ -48,10 +55,20 @@ onload = () => {
             listTasks.innerHTML = ""
         }
     }
+
+    function removeSpecificTask(i){
+        if(i != null){
+            let index = parseInt(i.getAttribute("value"))
+            tasks = JSON.parse(localStorage.tasks)
+            tasks.splice(index, 1)
+            localStorage.tasks = JSON.stringify(tasks)
+        }
+        showTasks()
+    }   
     
     function check(checkbox){
         if(checkbox.value){
-            let id = parseInt(checkbox.value) - 1
+            let id = parseInt(checkbox.value)
             tasks = JSON.parse(localStorage.tasks)
             if(tasks[id]["checkbox"]){
                 tasks[id]["checkbox"] = false
@@ -70,7 +87,7 @@ onload = () => {
                 tasks = JSON.parse(localStorage.tasks)
 
                 let task = {
-                    "id": tasks.length + 1,
+                    "id": tasks.length,
                     "checkbox": false,
                     "task": inputTask.value
                 }
@@ -79,7 +96,7 @@ onload = () => {
                 localStorage.tasks = JSON.stringify(tasks)
             }else{
                 let task = {
-                    "id": 1,
+                    "id": 0,
                     "checkbox": false,
                     "task": inputTask.value
                 }
